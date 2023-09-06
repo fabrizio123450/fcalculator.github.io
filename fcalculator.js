a1d30af32ca0c472d845debbb50c030b4181fd5e
unknown <fabrizio123450@gmail.com>
2023-09-06 14:48:45 -0400

    pequeños cambios

--- fcalculator.js
+++ fcalculator.js
@@ -1,21 +1,37 @@
+let operacionActual = "";
+let resultado = document.getElementById("pantalla");
  // Funciones JavaScript para la calculadora
- function agregarNumero(numero) {
-    document.getElementById("pantalla").value += numero;
+ function agregarCaracter(caracter) {
+    // Verificar si el último carácter en la operación actual es un operador
+    const ultimoCaracter = operacionActual.charAt(operacionActual.length - 1);
+    
+    if (esOperador(ultimoCaracter) && esOperador(caracter)) {
+        return;
+    }
+    
+    // Agregar el caracter a la operación actual
+    operacionActual += caracter;
+    document.getElementById("pantalla").value += caracter;
 }
-
-function operacion(operador) {
-    document.getElementById("pantalla").value += operador;
+// devuelve los operadores
+function esOperador(caracter) {
+    return ['+', '-', '*', '/','.'].includes(caracter);
 }
 
 function borrar() {
     document.getElementById("pantalla").value = "";
+    operacionActual = "";
 }
 
 function calcular() {
     try {
         const resultado = eval(document.getElementById("pantalla").value);
         document.getElementById("pantalla").value = resultado;
+        operacionActual = "";
     } catch (error) {
         document.getElementById("pantalla").value = "Error";
     }
 }
+
+
+
