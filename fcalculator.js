a3853245fe3488f1437db79eb7303b367762a897
unknown <fabrizio123450@gmail.com>
2023-09-07 03:54:53 -0400

    calculadora alpha 1

--- fcalculator.js
+++ fcalculator.js
@@ -13,19 +13,42 @@ let resultado = document.getElementById("pantalla");
     operacionActual += caracter;
     document.getElementById("pantalla").value += caracter;
 }
+//calcula la funcion trigonometrica 
+function agregarFuncion(sfuncion){
+    if(sfuncion === "sin"){
+        const expresion = document.getElementById("pantalla").value;
+        const resultado = Math.sin(eval(expresion));
+        const resultadoRedondeado = resultado.toFixed(3); // Reducir a 3 decimales
+        document.getElementById("pantalla").value = resultadoRedondeado;
+    }
+    if(sfuncion === "cos"){
+        const expresion = document.getElementById("pantalla").value;
+        const resultado = Math.cos(eval(expresion));
+        const resultadoRedondeado = resultado.toFixed(3); 
+        document.getElementById("pantalla").value = resultadoRedondeado;
+     }
+     if(sfuncion === "tan"){
+        const expresion = document.getElementById("pantalla").value;
+        const resultado = Math.tan(eval(expresion));
+        const resultadoRedondeado = resultado.toFixed(3);
+        document.getElementById("pantalla").value = resultadoRedondeado;
+     }
+}
 // devuelve los operadores
 function esOperador(caracter) {
     return ['+', '-', '*', '/','.'].includes(caracter);
+    
 }
 
+//limpia pantalla
 function borrar() {
     document.getElementById("pantalla").value = "";
     operacionActual = "";
 }
-
+//resultado
 function calcular() {
     try {
-        const resultado = eval(document.getElementById("pantalla").value);
+        const resultado = eval(document.getElementById("pantalla").value).toFixed(3);
         document.getElementById("pantalla").value = resultado;
         operacionActual = "";
     } catch (error) {
